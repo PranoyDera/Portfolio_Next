c5a3f8734896395791a3d1a6cbacde71856b0882
Pranoy Dera <pranoy.dera@nzminds.com>
2025-09-16 13:57:14 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-[#111] w-full h-screen">
+           {children}
+        </div>
+       
       </body>
     </html>
   );
