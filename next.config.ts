35fb030fe8b7832c5dde38e6bfcb9f24354dfb66
Pranoy Dera <pranoy.dera@nzminds.com>
2025-09-17 18:00:22 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://192.168.0.134:3000"],
+  experimental: {
+    // your other experimental stuff
+  },
 };
 
 export default nextConfig;
